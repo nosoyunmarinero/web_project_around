62817d60a1b9f326e7a0df1dd64f35fac9b21e70
Francisco Romero <nosoyunmarinero@gmail.com>
2024-07-29 21:24:20 -0600

    Se cambio la ref de las imagenes 3

--- js/script.js
+++ js/script.js
@@ -79,9 +79,9 @@ likeButtons.forEach((button) => {
     const heartIcon = button.querySelector("img");
 
     if (heartIcon.src.includes("heart.svg")) {
-      heartIcon.src = "/images/heartOn.svg";
+      heartIcon.src = "./images/heartOn.svg";
     } else {
-      heartIcon.src = "/images/heart.svg";
+      heartIcon.src = "./images/heart.svg";
     }
   });
 });
