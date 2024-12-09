c015eca0e5a63e6b9aeee9d87659554e3cace63b
Francisco Romero <nosoyunmarinero@gmail.com>
2024-12-08 20:20:01 -0600

    HTML modificado para template y creacion de clase card

--- js/Card.js
+++ js/Card.js
@@ -1,8 +1,31 @@
-class Card {
+export class Card {
   constructor(data, templateSelector) {
     this._image = image;
     this._title = title;
   }
 
-  _getTemplate() {}
+  _getTemplate() {
+    document
+      .querySelector(".elements")
+      .content.querySelector(".element")
+      .cloneNode(true);
+
+    return cardElement;
+  }
+
+  generateCard() {
+    this._element = this._getTemplate;
+
+    this._element.getElementById("card__title").textContent = this._title;
+    this._element.querySelector(".element__image").src = `${this._image}`;
+
+    return this._element;
+  }
 }
+
+item.forEach((item) => {
+  const card = new Card(item.image, item.title);
+  const cardElement = card.generateCard();
+
+  document.querySelector(".element-list__item").append(cardElement);
+});
