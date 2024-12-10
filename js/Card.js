826dc8818dfeb69cb51a25cecf6869cf0443313d
Francisco Romero <nosoyunmarinero@gmail.com>
2024-12-09 18:08:25 -0600

    Se creo la clase Card y se remplazo la funcion para agregar cards iniciales

--- js/Card.js
+++ js/Card.js
@@ -1,31 +1,27 @@
+/* Cards iniciales */
 export class Card {
   constructor(data, templateSelector) {
-    this._image = image;
-    this._title = title;
+    this._image = data.image;
+    this._title = data.title;
+    this._templateSelector = templateSelector;
   }
 
   _getTemplate() {
-    document
-      .querySelector(".elements")
-      .content.querySelector(".element")
+    const template = document.querySelector(this._templateSelector);
+
+    const cardElement = template.content
+      .querySelector(".element")
       .cloneNode(true);
 
     return cardElement;
   }
 
   generateCard() {
-    this._element = this._getTemplate;
+    this._element = this._getTemplate();
 
-    this._element.getElementById("card__title").textContent = this._title;
+    this._element.querySelector(".element__title").textContent = this._title;
     this._element.querySelector(".element__image").src = `${this._image}`;
 
     return this._element;
   }
 }
-
-item.forEach((item) => {
-  const card = new Card(item.image, item.title);
-  const cardElement = card.generateCard();
-
-  document.querySelector(".element-list__item").append(cardElement);
-});
