e2e0bf74316698a12e3377c6cc17ed08efd3066b
FranciscoRV <nosoyunmarinero@gmail.com>
2025-03-09 22:21:53 -0600

    Se corrigio el bug del boton de like y ahora funciona correctamente

--- js/Card.js
+++ js/Card.js
@@ -1,28 +1,20 @@
-/* Cards iniciales */
 export class Card {
   constructor(data, templateSelector, handleCardClick) {
     this._image = data.link;
     this._title = data.name;
     this._id = data._id;
-    this.isLiked = false;
+    this.isLiked = data.isLiked;
     this._templateSelector = templateSelector;
     this._handleCardClick = handleCardClick;
   }
 
-  //Cambiar like
+  // Cambiar like
   toggleLike() {
     this.isLiked = !this.isLiked;
-    const likeIcon = this._element.querySelector(".element__like-button");
-    if (this.isLiked) {
-      likeIcon.src = "./images/heart-on.svg";
-    } else {
-      likeIcon.src = "./images/heart.svg";
-    }
   }
 
   _getTemplate() {
     const template = document.querySelector(this._templateSelector);
-
     const cardElement = template.content
       .querySelector(".element")
       .cloneNode(true);
@@ -32,61 +24,62 @@ export class Card {
 
   generateCard() {
     this._element = this._getTemplate();
-    //Title
+    // Title
     this._element.querySelector(".element__title").textContent = this._title;
-    //Image
+    // Image
     const imgElement = this._element.querySelector(".element__image");
     imgElement.src = this._image;
+    imgElement.alt = this._title;
     // Id
     this._element.id = this._id;
 
-    //Like
+    // Like button
     const likeButton = this._element.querySelector(".element__button-like");
     const likeIcon = this._element.querySelector(".element__like-button");
 
-    //Like button
+    // Set initial state of like button
+    this.updateLikeButtonState(likeIcon);
+
+    // Like button event listener
     likeButton.addEventListener("click", () => {
       this.toggleLike();
 
-      if (this.isLiked) {
-        fetch(
-          `https://around-api.es.tripleten-services.com/v1/cards/${this._id}/likes`,
-          {
-            method: "PUT",
-            headers: {
-              authorization: "354781f2-b486-4ab1-9379-468b53f9329e",
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              isLiked: true,
-            }),
-          }
-        )
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data.isLiked);
-          })
-          .catch((err) =>
-            console.error("Error al dar like a la tarjeta:", err)
+      const method = this.isLiked ? "PUT" : "DELETE";
+      const body = this.isLiked ? JSON.stringify({ isLiked: true }) : null;
+
+      fetch(
+        `https://around-api.es.tripleten-services.com/v1/cards/${this._id}/likes`,
+        {
+          method,
+          headers: {
+            authorization: "354781f2-b486-4ab1-9379-468b53f9329e",
+            "Content-Type": "application/json",
+          },
+          body,
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          this.isLiked = data.isLiked;
+          this.updateLikeButtonState(likeIcon);
+          console.log(data.isLiked);
+        })
+        .catch((err) => {
+          console.error(
+            `Error al ${this.isLiked ? "dar" : "quitar"} like a la tarjeta:`,
+            err
           );
-      } else {
-        fetch(
-          `https://around-api.es.tripleten-services.com/v1/cards/${this._id}/likes`,
-          {
-            method: "DELETE",
-            headers: { authorization: "354781f2-b486-4ab1-9379-468b53f9329e" },
-          }
-        )
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data.isLiked);
-          })
-          .catch((err) =>
-            console.error("Error al quitar like a la tarjeta:", err)
-          );
-      }
+        });
     });
 
     return this._element;
+  }
+
+  updateLikeButtonState(likeIcon) {
+    if (this.isLiked) {
+      likeIcon.src = "./images/heart-on.svg";
+    } else {
+      likeIcon.src = "./images/heart.svg";
+    }
   }
 }
